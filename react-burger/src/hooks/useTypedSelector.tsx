4ad6458977b0cf1865076a1426c8c4d9871a71c0
KozachukAP <aleksandr.kozachuk@stm-labs.ru>
2024-09-25 12:05:58 +0200

    Updates

--- react-burger/src/hooks/useTypedSelector.tsx
+++
@@ -1,8 +0,0 @@
-import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
-import {RootState} from "../services/init";
-import {UnknownAction} from "redux";
-import {ThunkDispatch} from "redux-thunk";
-
-export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppDispatch = () => useDispatch<ThunkDispatch<RootState, unknown, UnknownAction>>();
-export type AppDispatch = ThunkDispatch<RootState, unknown, UnknownAction>;
